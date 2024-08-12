2fb0ad9803b4b7aca22e0faf43957e5de0bb28c9
Teodora Saneva <73078235+sanevat@users.noreply.github.com>
2024-08-12 13:45:14 +0200

    Refactor

--- src/F1Test.java
+++ src/F1Test.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+
 class TimeClass implements Comparable<TimeClass> {
     private int minutes;
     private int seconds;
@@ -14,82 +15,91 @@ class TimeClass implements Comparable<TimeClass> {
         this.seconds = seconds;
         this.miliseconds = miliseconds;
     }
-    public long totalMiliSeconds(){
-        return minutes*60*1000+seconds*1000+miliseconds;
+
+    public long totalMiliSeconds() {
+        return minutes * 60 * 1000 + seconds * 1000 + miliseconds;
     }
 
     @Override
     public int compareTo(TimeClass o) {
-        return Long.compare(this.totalMiliSeconds(),o.totalMiliSeconds());
+        return Long.compare(this.totalMiliSeconds(), o.totalMiliSeconds());
     }
 
     @Override
     public String toString() {
-        return String.format("%d:%02d:%03d",minutes,seconds,miliseconds);
+        return String.format("%d:%02d:%03d", minutes, seconds, miliseconds);
     }
 }
-class Driver implements Comparable<Driver>{
+
+class Driver implements Comparable<Driver> {
     private String name;
-    private List<TimeClass>times;
+    private List<TimeClass> times;
+
     public Driver(String name, List<TimeClass> times) {
         this.name = name;
         this.times = times;
     }
-    public static Driver createDriver(String line){
-        String []parts=line.split("\\s+");
-        String name=parts[0];
 
-        List<TimeClass> times =new ArrayList<>();
+    public static Driver createDriver(String line) {
+        String[] parts = line.split("\\s+");
+        String name = parts[0];
+
+        List<TimeClass> times = new ArrayList<>();
         Arrays.stream(parts)
                 .skip(1)
                 .forEach(timeInput -> {
-                    String[]timeParts=timeInput.split(":");
-                    int minute=Integer.parseInt(timeParts[0]);
-                    int second=Integer.parseInt(timeParts[1]);
-                    int mili=Integer.parseInt(timeParts[2]);
-                    times.add(new TimeClass(minute,second,mili));
+                    String[] timeParts = timeInput.split(":");
+                    int minute = Integer.parseInt(timeParts[0]);
+                    int second = Integer.parseInt(timeParts[1]);
+                    int mili = Integer.parseInt(timeParts[2]);
+                    times.add(new TimeClass(minute, second, mili));
                 });
 
-        return new Driver(name,times);
+        return new Driver(name, times);
     }
-    public TimeClass getMaxTime(){
-        times= times.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+
+    public TimeClass getMaxTime() {
+        times = times.stream().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
         return times.get(0);
     }
 
     @Override
     public String toString() {
-        return String.format("%-10s %s",name,getMaxTime().toString());
+        return String.format("%-10s %s", name, getMaxTime().toString());
     }
 
     @Override
     public int compareTo(Driver o) {
-        return Long.compare(this.getMaxTime().totalMiliSeconds(),o.getMaxTime().totalMiliSeconds());
+        return Long.compare(this.getMaxTime().totalMiliSeconds(), o.getMaxTime().totalMiliSeconds());
     }
 }
-class F1Race{
-    List<Driver>drivers;
+
+class F1Race {
+    List<Driver> drivers;
 
     public F1Race() {
-        this.drivers=new ArrayList<>();
+        this.drivers = new ArrayList<>();
     }
-    public void readResults(InputStream in){
-        BufferedReader br=new BufferedReader(new InputStreamReader(in));
-        drivers=br.lines()
+
+    public void readResults(InputStream in) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        drivers = br.lines()
                 .map(Driver::createDriver)
                 .collect(Collectors.toList());
     }
+
     public void printSorted(PrintStream out) {
-        PrintWriter pw=new PrintWriter(out);
+        PrintWriter pw = new PrintWriter(out);
         drivers.sort(Comparator.naturalOrder());
-        int id=1;
-        for (Driver d:drivers) {
-            pw.println(id+". "+d.toString());
+        int id = 1;
+        for (Driver d : drivers) {
+            pw.println(id + ". " + d.toString());
             id++;
         }
         pw.flush();
     }
 }
+
 public class F1Test {
 
     public static void main(String[] args) {
