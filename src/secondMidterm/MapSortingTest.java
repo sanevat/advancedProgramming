3f50a65df8602fcff95aad7c08f2cc549d4725ff
Teodora Saneva <73078235+sanevat@users.noreply.github.com>
2024-08-31 22:21:43 +0200

    Add file

--- src/secondMidterm/MapSortingTest.java
+++ src/secondMidterm/MapSortingTest.java
@@ -2,9 +2,9 @@ package secondMidterm;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.*;
 
 public class MapSortingTest {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -21,17 +21,18 @@ public class MapSortingTest {
             SortedSet<Map.Entry<Integer, String>> s = entriesSortedByValues(map);
             System.out.println(s);
         }
+
     }
 
-    public static <T extends Comparable<T>, V extends Comparable<V>> SortedSet<Map.Entry<T, V>>entriesSortedByValues(Map<T, V> map){
+    private static <T,V extends Comparable<V>> SortedSet<Map.Entry<T, V>> entriesSortedByValues(Map<T, V> map) {
+        System.out.println(map);
         Comparator<Map.Entry<T,V>> comparator = (a,b)->{
             if (a.getValue().compareTo(b.getValue())==0)return 1;
             else return a.getValue().compareTo(b.getValue());
         };
-        System.out.println(map);
-        return map.entrySet().stream()
-                .sorted(comparator.reversed())
-                .collect(Collectors.toCollection(()->new TreeSet<>(comparator.reversed())));
+        SortedSet<Map.Entry<T,V>>sortedSet=new TreeSet<>(comparator.reversed());
+                map.entrySet().forEach(sortedSet::add);
+                return sortedSet;
 
     }
 
@@ -48,5 +49,6 @@ public class MapSortingTest {
     static void fillIntegerStringMap(List<String> l, Map<Integer, String> map) {
         l.forEach(s -> map.put(Integer.parseInt(s.substring(0, s.indexOf(':'))), s.substring(s.indexOf(':') + 1)));
     }
+
 
 }
